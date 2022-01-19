5e9f2e42f121686179f082a630892bd3a46577b3
Benjamin Semah <benjaminsemah@gmail.com>
2022-01-19 04:42:05 +0000

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -43,17 +43,3 @@ todoItems.forEach((item) => {
 const todoContainer = document.querySelector('.todo-list');
 
 todoContainer.innerHTML = todoHTML;
-
-////////////////////////////////////////////////
-// function component() {
-//   const element = document.createElement('div');
-
-//   // Lodash, now imported by this script
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//   element.classList.add('hello');
-  
-//   return element;
-// }
-
-// document.body.appendChild(component());
-// index.html
