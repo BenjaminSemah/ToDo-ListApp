d877439f5f8cef8cac663ad6da853ab6cf0869c1
Benjamin Semah <benjaminsemah@gmail.com>
2022-01-19 04:27:24 +0000

    Generate To Do task dynamically from index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,59 @@
 import _ from 'lodash';
 import './style.css';
+import dotsIcon from './imgDots.svg';
 
-function component() {
-  const element = document.createElement('div');
+const todoItems = [
+  {
+    description: 'Go for a run',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Read 5 pages',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Complete todo list project',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Spend 15 minutes on LinkedIn',
+    completed: false,
+    index: 4,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+let todoHTML = '';
+
+todoItems.forEach((item) => {
+  todoHTML
+    += `
+      <li class="list-item">
+      <div class="checkAndText">
+        <input id="${item.completed}" class="checkbox" type="checkbox" />
+        <p class="todo-text" id="${item.index}">${item.description}</p>
+      </div>
+      <img id="dots-image" src="${dotsIcon}" alt="three dots image" />
+      </li>
+   `;
+});
+
+const todoContainer = document.querySelector('.todo-list');
+
+todoContainer.innerHTML = todoHTML;
+
+////////////////////////////////////////////////
+// function component() {
+//   const element = document.createElement('div');
+
+//   // Lodash, now imported by this script
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   element.classList.add('hello');
   
-  return element;
-}
+//   return element;
+// }
 
-document.body.appendChild(component());
-index.html
+// document.body.appendChild(component());
+// index.html
